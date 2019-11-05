7b31a1a70b5f3a6a5650c09eb05d3c6fd4abbf7d
trinhhuuhieu99@gmail.com <trinhhuuhieu99@gmail.com>
2019-11-05 23:43:18 +0700

    add helloworld 2

--- JavaApplication16/src/javaapplication16/JavaApplication16.java
+++ JavaApplication16/src/javaapplication16/JavaApplication16.java
@@ -16,7 +16,7 @@ public class JavaApplication16 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello world 1");
+        System.out.println("hello world 2");
     }
     
 }
